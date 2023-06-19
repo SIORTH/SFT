bcb26d9bf4715b3a904c59a3f626e942e654dd57
MUFA <fajars.tech@gmail.com>
2023-06-19 16:43:14 +0700

    first commit

---
+++ assets/js/form.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const nodemailer = require('nodemailer');
+
+app.use(express.json());
+
+app.post('/send-email', (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+  // Konfigurasi transport Nodemailer
+  const transporter = nodemailer.createTransport({
+    // Konfigurasi SMTP atau layanan email yang sesuai
+    // ...
+  });
+
+  // Konten email
+  const emailContent = {
+    from: email,
+    to: 'fajars.tech@gmail.com', // Ganti dengan alamat email penerima yang sesuai
+    subject: subject,
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+  };
+
+  // Mengirim email
+  transporter.sendMail(emailContent, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Failed to send email.' });
+    } else {
+      console.log('Email sent:', info.response);
+      res.json({ message: 'Email sent successfully.' });
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
